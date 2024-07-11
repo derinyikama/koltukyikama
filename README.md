ff8a38f4c8524712787be649577ec36e5e57aa97
derinyikama <derinyikamacomtr@gmail.com>
2024-07-11 22:32:37 +0300

    koltuk yikama

--- README.md
+++ README.md
@@ -1,36 +1,24 @@
-This is a [Next.js](https://nextjs.org/) project bootstrapped with [`create-next-app`](https://github.com/vercel/next.js/tree/canary/packages/create-next-app).
+Bu proje derin yıkamanın hizmetleri ve bölgeleri için tasarlanmıştır.
 
-## Getting Started
+## Derin Koltuk Yıkama
+### Hizmetler
+- [Koltuk Yıkama](http://derinyikama.com.tr)
+- [Yatak Yıkama](http://derinyikama.com.tr)
+- [Sandalye Yıkama](http://derinyikama.com.tr)
+- [Halıfleks Yıkama](http://derinyikama.com.tr)
 
-First, run the development server:
-
-```bash
-npm run dev
-# or
-yarn dev
-# or
-pnpm dev
-# or
-bun dev
-```
-
-Open [http://localhost:3000](http://localhost:3000) with your browser to see the result.
-
-You can start editing the page by modifying `app/page.tsx`. The page auto-updates as you edit the file.
-
-This project uses [`next/font`](https://nextjs.org/docs/basic-features/font-optimization) to automatically optimize and load Inter, a custom Google Font.
-
-## Learn More
-
-To learn more about Next.js, take a look at the following resources:
-
-- [Next.js Documentation](https://nextjs.org/docs) - learn about Next.js features and API.
-- [Learn Next.js](https://nextjs.org/learn) - an interactive Next.js tutorial.
-
-You can check out [the Next.js GitHub repository](https://github.com/vercel/next.js/) - your feedback and contributions are welcome!
-
-## Deploy on Vercel
-
-The easiest way to deploy your Next.js app is to use the [Vercel Platform](https://vercel.com/new?utm_medium=default-template&filter=next.js&utm_source=create-next-app&utm_campaign=create-next-app-readme) from the creators of Next.js.
-
-Check out our [Next.js deployment documentation](https://nextjs.org/docs/deployment) for more details.
+### Bölgeler
+- [İstanbul Koltuk Yıkama](https://derinyikama.com.tr/istanbul-koltuk-yikama)
+- [Maltepe Koltuk Yıkama](https://derinyikama.com.tr/maltepe-koltuk-yikama)
+- [Kadıköy Koltuk Yıkama](https://derinyikama.com.tr/kadikoy-koltuk-yikama)
+- [Kartal Koltuk Yıkama](https://derinyikama.com.tr/kartal-koltuk-yikama)
+- [Üsküdar Koltuk Yıkama](https://derinyikama.com.tr/uskudar-koltuk-yikama)
+- [Sancaktepe Koltuk Yıkama](https://derinyikama.com.tr/sancaktepe-koltuk-yikama)
+- [Ataşehir Koltuk Yıkama](https://derinyikama.com.tr/atasehir-koltuk-yikama)
+- [Adalar Koltuk Yıkama](https://derinyikama.com.tr/adalar-koltuk-yikama)
+- [Beykoz Koltuk Yıkama](https://derinyikama.com.tr/beykoz-koltuk-yikama)
+- [Çekmeköy Koltuk Yıkama](https://derinyikama.com.tr/cekmekoy-koltuk-yikama)
+- [Pendik Koltuk Yıkama](https://derinyikama.com.tr/pendik-koltuk-yikama)
+- [Şile Koltuk Yıkama](https://derinyikama.com.tr/sile-koltuk-yikama)
+- [Tuzla Koltuk Yıkama](https://derinyikama.com.tr/tuzla-koltuk-yikama)
+- [Ümraniye Koltuk Yıkama](https://derinyikama.com.tr/umraniye-koltuk-yikama)
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,20 +5,19 @@ export default function Home() {
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
         <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.tsx</code>
+          Her zaman en iyi koltuk yıkama&nbsp;
+          <code className="font-mono font-bold">Derin Koltuk Yıkama</code>
         </p>
         <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:size-auto lg:bg-none">
           <a
             className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
+            href="https://derinyikama.com.tr/"
             target="_blank"
-            rel="noopener noreferrer"
+            title="Koltuk Yıkama"
           >
-            By{" "}
             <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
+              src="https://derinyikama.com.tr/images/logo.png"
+              alt="Koltuk Yıkama"
               className="dark:invert"
               width={100}
               height={24}
@@ -28,83 +27,91 @@ export default function Home() {
         </div>
       </div>
 
-      <div className="relative z-[-1] flex place-items-center before:absolute before:h-[300px] before:w-full before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 sm:before:w-[480px] sm:after:w-[240px] before:lg:h-[360px]">
+      <div className="relative z-[-1] content-center place-items-center before:absolute before:h-[300px] before:w-full before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 sm:before:w-[480px] sm:after:w-[240px] before:lg:h-[360px]">
+        <h1  className="mb-3 text-2xl font-semibold">Koltuk Yıkama</h1>
         <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
+        src="https://derinyikama.com.tr/imagest/crop-jpg-w550xh550-homeimage.webp"
+        width={550}
+        height={550}
+        ></Image>
+        <div className="w-full">
+          <ul>
+            <li> Buharlı Koltuk Yıkama </li>
+            <li> Hızlı ve Detaylı Yıkama </li>
+            <li> Aynı Gün Kullanım </li>
+            <li> Kredi Kartı ile Ödeme </li>
+            <li> 7/24 Hizmet </li>
+          </ul>
+        </div>
+        <ul> <li> <a href="https://derinyikama.com.tr/adalar-koltuk-yikama" title="Adalar Koltuk Yıkama">  Adalar Koltuk Yıkama</a> </li> <li> <a href="https://derinyikama.com.tr/atasehir-koltuk-yikama" title="Ataşehir Koltuk Yıkama">  Ataşehir Koltuk Yıkama</a> </li> <li> <a href="https://derinyikama.com.tr/beykoz-koltuk-yikama" title="Beykoz Koltuk Yıkama">  Beykoz Koltuk Yıkama</a> </li> <li> <a href="https://derinyikama.com.tr/cekmekoy-koltuk-yikama" title="Çekmeköy Koltuk Yıkama">  Çekmeköy Koltuk Yıkama</a> </li> <li> <a href="https://derinyikama.com.tr/istanbul-koltuk-yikama" title="İstanbul Koltuk Yıkama">  İstanbul Koltuk Yıkama</a> </li> <li> <a href="https://derinyikama.com.tr/kadikoy-koltuk-yikama" title="Kadıköy Koltuk Yıkama">  Kadıköy Koltuk Yıkama</a> </li> <li> <a href="https://derinyikama.com.tr/kartal-koltuk-yikama" title="Kartal Koltuk Yıkama">  Kartal Koltuk Yıkama</a> </li> <li> <a href="https://derinyikama.com.tr/maltepe-koltuk-yikama" title="Maltepe Koltuk Yıkama">  Maltepe Koltuk Yıkama</a> </li> <li> <a href="https://derinyikama.com.tr/pendik-koltuk-yikama" title="Pendik Koltuk Yıkama">  Pendik Koltuk Yıkama</a> </li> <li> <a href="https://derinyikama.com.tr/sancaktepe-koltuk-yikama" title="Sancaktepe Koltuk Yıkama">  Sancaktepe Koltuk Yıkama</a> </li> <li> <a href="https://derinyikama.com.tr/sile-koltuk-yikama" title="Şile Koltuk Yıkama">  Şile Koltuk Yıkama</a> </li> <li> <a href="https://derinyikama.com.tr/tuzla-koltuk-yikama" title="Tuzla Koltuk Yıkama">  Tuzla Koltuk Yıkama</a> </li> <li> <a href="https://derinyikama.com.tr/umraniye-koltuk-yikama" title="Ümraniye Koltuk Yıkama">  Ümraniye Koltuk Yıkama</a> </li> <li> <a href="https://derinyikama.com.tr/uskudar-koltuk-yikama" title="Üsküdar Koltuk Yıkama">  Üsküdar Koltuk Yıkama</a> </li> </ul>
       </div>
 
       <div className="mb-32 grid text-center lg:mb-0 lg:w-full lg:max-w-5xl lg:grid-cols-4 lg:text-left">
         <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
+          href="https://derinyikama.com.tr/"
           className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
           target="_blank"
-          rel="noopener noreferrer"
+          title="Koltuk Yıkama"
         >
           <h2 className="mb-3 text-2xl font-semibold">
-            Docs{" "}
+            Koltuk Yıkama{" "}
             <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
               -&gt;
             </span>
           </h2>
           <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Find in-depth information about Next.js features and API.
+            Profesyonel derin koltuk yıkama hizmeti
           </p>
         </a>
 
         <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
+          href="https://derinyikama.com.tr/"
           className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
           target="_blank"
-          rel="noopener noreferrer"
+          title="Yatak Yıkama"
         >
           <h2 className="mb-3 text-2xl font-semibold">
-            Learn{" "}
+            Yatak Yıkama{" "}
             <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
               -&gt;
             </span>
           </h2>
           <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
+            Profesyonel derin yatak yıkama hizmeti
           </p>
         </a>
 
         <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
+          href="https://derinyikama.com.tr/"
           className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
           target="_blank"
-          rel="noopener noreferrer"
+          title="Sandalye Yıkama"
         >
           <h2 className="mb-3 text-2xl font-semibold">
-            Templates{" "}
+            Sandalye Yıkama{" "}
             <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
               -&gt;
             </span>
           </h2>
           <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Explore starter templates for Next.js.
+            Profesyonel derin sandalye yıkama hizmeti
           </p>
         </a>
 
         <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
+          href="https://derinyikama.com.tr/"
           className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
           target="_blank"
-          rel="noopener noreferrer"
+          title="Halıfleks Yıkama"
         >
           <h2 className="mb-3 text-2xl font-semibold">
-            Deploy{" "}
+            Halıfleks Yıkama{" "}
             <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
               -&gt;
             </span>
           </h2>
           <p className="m-0 max-w-[30ch] text-balance text-sm opacity-50">
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
+            Profesyonel derin halıfleks yıkama hizmeti
           </p>
         </a>
       </div>
